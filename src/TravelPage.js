4bd0576211c31f525ff97a9e9e6dce2c1264d755
Bethany Drake <bethanyd@thoughtworks.com>
2019-01-15 14:37:21 +1100

    add travel time

--- src/TravelPage.js
+++ src/TravelPage.js
@@ -31,16 +31,35 @@ class TravelButton extends Component {
     super(props)
     console.log(this.props)
     this.go = this.go.bind(this)
+
   }
   go(){
+    let currentLocation = this.props.data.currentLocation
     console.log("going", this.props.data.currentLocation, this.props.city)
     this.props.data.currentLocation = this.props.city
-    this.props.data.currentDate = this.props.data.currentDate.add(1, 'd')
+    let distance = locations[currentLocation].connections[this.props.city]
+    this.props.data.currentDate = this.props.data.currentDate.add(distance, 'd')
     if (this.props.data.locationManager) this.props.data.locationManager.updateState();
   }
   render(){
+    let currentLocation = this.props.data.currentLocation
+    var distance = locations[currentLocation].connections[this.props.city]
+    let isCurrentCity = this.props.city===this.props.data.currentLocation
+    if (isCurrentCity) distance = 0;
+    var unit = "days"
+    if (distance == 1) unit = "day"
+    var toWrite;
+    if (distance === undefined) {
+      unit = ""
+      toWrite = ""
+    }else{
+      toWrite = " ("+distance + ' ' + unit +")"
+    }
+    let reachable = distance !== undefined ? true : false
+
+    //if (!reachable || isCurrentCity) return(<></>)
     return(
-    <Button disabled={this.props.city===this.props.data.currentLocation} onClick={this.go}> {this.props.city} </Button>
+    <Button disabled={!reachable || isCurrentCity} onClick={this.go}> {this.props.city}{toWrite} </Button>
   )
   }
 }
--- src/locations.js
+++ src/locations.js
@@ -1,37 +1,45 @@
 export const locations = {
   'Melbourne':{
-  calcPrice:(cat) => cat.cuteness/5
+  calcPrice:(cat) => cat.cuteness/5,
+  connections: {'Canberra':1, 'Adelaide':1, 'Launceston':2}
 },
 
-'Geelong':{
-calcPrice:(cat) => cat.affection/5
-},
 'Adelaide':{
-calcPrice:(cat) => cat.intelligence/5
+calcPrice:(cat) => cat.intelligence/5,
+connections:{'Perth':6, 'Alice Springs':5, 'Melbourne':1}
 },
 'Brisbane':{
-calcPrice:(cat) => cat.aggression/5
+calcPrice:(cat) => cat.aggression/5,
+connections:{'Sydney':3}
 },
 'Perth':{
-calcPrice:(cat) => cat.energy/5
+calcPrice:(cat) => cat.energy/5,
+connections:{'Alice Springs': 5, 'Adelaide': 6}
+
 },
 'Darwin':{
-calcPrice:(cat) => 1
+calcPrice:(cat) => 1,
+connections:{}
 },
 'Alice Springs':{
-calcPrice:(cat) => cat.cuteness/7
+calcPrice:(cat) => cat.cuteness/7,
+connections:{'Perth':5, 'Adelaide':5,}
 },
 'Sydney':{
-calcPrice:(cat) => cat.affection/4
+calcPrice:(cat) => cat.affection/4,
+connections:{'Canberra':1, 'Brisbane':3}
 },
 'Canberra':{
-calcPrice:(cat) => cat.cuteness/4
+calcPrice:(cat) => cat.cuteness/4,
+connections:{'Sydney':1, 'Melbourne':2 }
 },
 'Hobart':{
-calcPrice:(cat) => cat.intelligence/2
+calcPrice:(cat) => cat.intelligence/2,
+connections:{'Launceston':1}
 },
 'Launceston':{
-calcPrice:(cat) => cat.intelligence/8
+calcPrice:(cat) => cat.intelligence/8,
+connections:{'Hobart':1, 'Melbourne': 2}
 }
 
 }
